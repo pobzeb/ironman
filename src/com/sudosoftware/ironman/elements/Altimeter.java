d646b908d18176c7f22db05ad730a61d6b0900d9
pobzeb@gmail.com <pobzeb@gmail.com>
2014-04-24 22:16:47 -0600

    Update to altimeter display. Still working on value.

--- src/com/sudosoftware/ironman/elements/Altimeter.java
+++ src/com/sudosoftware/ironman/elements/Altimeter.java
@@ -7,6 +7,8 @@ import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 
+import com.sudosoftware.ironman.gltext.GLText;
+import com.sudosoftware.ironman.gltext.GLTextFactory;
 import com.sudosoftware.ironman.shapes.BezierCurve;
 import com.sudosoftware.ironman.shapes.Circle;
 import com.sudosoftware.ironman.shapes.Point3D;
@@ -20,6 +22,9 @@ public class Altimeter extends HUDElement implements SensorEventListener {
 	// Hold the sensor info.
 	private SensorManager sensorManager;
 	private Sensor presure;
+
+	// GL Text for display.
+	private GLText glAltitudeText;
 
 	public Altimeter() {
 		super();
@@ -38,10 +43,15 @@ public class Altimeter extends HUDElement implements SensorEventListener {
 		sensorManager = SensorManagerFactory.getInstance().getManager();
 		presure = sensorManager.getDefaultSensor(Sensor.TYPE_PRESSURE);
 		registerListeners();
+
+		// Load the font.
+		glAltitudeText = GLTextFactory.getInstance().createGLText();
+		glAltitudeText.load("Roboto-Regular.ttf", 65, 2, 2);
+
 	}
 
 	private void registerListeners() {
-		sensorManager.registerListener(this, presure, SensorManager.SENSOR_DELAY_GAME);
+		sensorManager.registerListener(this, presure, SensorManager.SENSOR_DELAY_NORMAL);
 	}
 
 	@Override
@@ -63,14 +73,38 @@ public class Altimeter extends HUDElement implements SensorEventListener {
 		ColorPicker.setGLColor(gl, ColorPicker.NEONBLUE, 0.75f);
 		for (float yLine = 300.0f; yLine >= -300.0f; yLine-=50.0f) {
 			// Skip the area where we want to show the current altitude.
-//			if (yLine < 51.0f && yLine > -51.0f) continue;
+			if (yLine < 51.0f && yLine > -51.0f) continue;
 			BezierCurve.draw2PointCurve(gl,
 				new Point3D(400.0f, yLine, 0.0f),
 				new Point3D(100.0f, yLine, 0.0f), GL10.GL_LINE_STRIP);
 		}
 		ColorPicker.setGLColor(gl, ColorPicker.BLACK, 0.0f);
 		Circle.drawCircle(gl, 335.0f, 500, GL10.GL_TRIANGLE_FAN);
+		ColorPicker.setGLColor(gl, ColorPicker.NEONBLUE, 0.75f);
+		BezierCurve.draw2PointCurve(gl,
+			new Point3D(340.0f, -50.0f, 0.0f),
+			new Point3D(340.0f,  50.0f, 0.0f), GL10.GL_LINE_STRIP);
+		BezierCurve.draw2PointCurve(gl,
+			new Point3D(740.0f, -50.0f, 0.0f),
+			new Point3D(740.0f,  50.0f, 0.0f), GL10.GL_LINE_STRIP);
+		BezierCurve.draw2PointCurve(gl,
+			new Point3D(340.0f,  50.0f, 0.0f),
+			new Point3D(740.0f,  50.0f, 0.0f), GL10.GL_LINE_STRIP);
+		BezierCurve.draw2PointCurve(gl,
+			new Point3D(340.0f, -50.0f, 0.0f),
+			new Point3D(740.0f, -50.0f, 0.0f), GL10.GL_LINE_STRIP);
 		gl.glLineWidth(1.0f);
+
+		// Draw the current date and time.
+		gl.glEnable(GL10.GL_TEXTURE_2D);
+		gl.glEnable(GL10.GL_BLEND);
+		gl.glBlendFunc(GL10.GL_SRC_ALPHA, GL10.GL_ONE_MINUS_SRC_ALPHA);
+		glAltitudeText.setScale(1.0f);
+		ColorPicker.setGLTextColor(glAltitudeText, ColorPicker.CORAL, 1.0f);
+		glAltitudeText.draw(altitude + " ft.", 740.0f - GLTextFactory.getStringWidth(glAltitudeText, altitude + " ft.") - 15.0f, -(glAltitudeText.getCharHeight() / 2.0f) + 5.0f);
+		glAltitudeText.end();
+		gl.glDisable(GL10.GL_BLEND);
+		gl.glDisable(GL10.GL_TEXTURE_2D);
 
 		gl.glPopMatrix();
 	}
@@ -93,7 +127,8 @@ public class Altimeter extends HUDElement implements SensorEventListener {
 	@Override
 	public void onSensorChanged(SensorEvent event) {
 		if (event.sensor.getType() == Sensor.TYPE_PRESSURE) {
-			// Do something.
+			// Get the altitude.
+	        altitude = SensorManager.getAltitude(SensorManager.PRESSURE_STANDARD_ATMOSPHERE, event.values[0]);
 		}
 	}
 
